5d8b389a43b2dd4994e638123fa773a7533ba201
Swift Studies <SwiftStudies@users.noreply.github.com>
2024-07-16 00:00:33 -0500

    DataPoint members were not public

--- Sources/TimeSeries/DataPoint.swift
+++ Sources/TimeSeries/DataPoint.swift
@@ -8,13 +8,13 @@ import Foundation
 /// A  value captured or calculated in a series at a certain point in time
 public struct DataPoint<T> {
     /// The value
-    let value   : T
+    public let value   : T
     
     /// Time in seconds since reference date when data point was captured
-    let timeInterval    : TimeInterval
+    public let timeInterval    : TimeInterval
     
     /// Date sample was taken
-    var date    : Date {
+    public var date    : Date {
         return Date(timeIntervalSinceReferenceDate: timeInterval)
     }
     
