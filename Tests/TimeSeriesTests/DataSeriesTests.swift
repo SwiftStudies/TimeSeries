cde5bf8d4e83ae604034ebfd9041c6ed173a8b1a
Swift Studies <SwiftStudies@users.noreply.github.com>
2024-07-21 17:05:06 -0500

    Added tests for EventSeries

---
+++ Tests/TimeSeriesTests/DataSeriesTests.swift
@@ -0,0 +1,39 @@
+//
+//  DataSeriesTests.swift
+//  TimeSeries
+//
+//
+
+import Testing
+@testable import TimeSeries
+import Foundation
+
+public enum TestEvent {
+    case personDetected, petDetected, vehicleDetected
+}
+
+@Test func testEventSeries() async throws {
+    var series = EventSeries<TestEvent>()
+    
+    try series.capture(.personDetected, at: 0)
+    try series.capture(.petDetected, at: 0)
+    try series.capture(.petDetected, at: 0.5)
+    try series.capture(.petDetected, at: 1.5)
+    try series.capture(.petDetected, at: 1.5)
+    try series.capture(.vehicleDetected, at: 2)
+    try series.capture(.personDetected, at: 2.5)
+    try series.capture(.personDetected, at: 3)
+    
+    #expect(series.timeRange == 0...3)
+    #expect(series[0].count == 2)
+    #expect(series[1.5].count == 2)
+    #expect(series[3].count == 1)
+    #expect(series[dataPointsFrom: 0...3].count == 8)
+    #expect(series[dataPointsFrom: 1...2-1.nanoseconds].count == 2)
+
+    series.clear()
+    #expect(series.dataPoints.count == 0)
+    #expect(series[2].count == 0)
+    #expect(series[dataPointsFrom: 0...3].count == 0)
+    #expect(series.timeRange == 0...0)
+}
