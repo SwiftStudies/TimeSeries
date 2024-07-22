9286e90c6bb12edcf4427ad6b98ddc55f7cad8ec
Swift Studies <SwiftStudies@users.noreply.github.com>
2024-07-22 09:24:44 -0500

    Updated to reflect not all data types are Samples

--- Sources/TimeSeries/Summarizers/Count.swift
+++ Sources/TimeSeries/Summarizers/Count.swift
@@ -1,5 +1,5 @@
 //
-//  CountSamples.swift
+//  Count.swift
 //  TimeSeries
 //
 //
@@ -7,9 +7,9 @@
 import Foundation
 
 /// Counts the number of samples in the period
-public struct CountSamples<S:Sampleable> : Summarizer {
+public struct Count<PointType> : Summarizer {
     public typealias DataType = Int
-    public typealias SourceType = S
+    public typealias SourceType = PointType
     
     public func summarize(series: any Series, for period: TimeInterval, startingAt start: TimeInterval) -> DataPoint<Int> {
         return DataPoint<Int>(value: series[dataPointsFrom: start...(start+period)].count, timeInterval: start)
--- Sources/TimeSeries/Summarizers/Summarizer.swift
+++ Sources/TimeSeries/Summarizers/Summarizer.swift
@@ -9,7 +9,7 @@ import Foundation
 /// Implementers of the protocol provide methods of generating a single data point from a `SampleSeries` base on a range of times in that data-series
 public protocol Summarizer<SourceType, DataType> {
     /// The types of sample in the source series
-    associatedtype SourceType : Sampleable
+    associatedtype SourceType 
     
     /// The type of the generated data type
     associatedtype DataType
