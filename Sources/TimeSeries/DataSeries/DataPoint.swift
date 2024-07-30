4e9e33f48557d5569c5c02718e03e61f7f8eb7e6
Swift Studies <SwiftStudies@users.noreply.github.com>
2024-07-29 21:40:10 -0500

    Made the initializer for DataPoint public

--- Sources/TimeSeries/DataSeries/DataPoint.swift
+++ Sources/TimeSeries/DataSeries/DataPoint.swift
@@ -18,5 +18,12 @@ public struct DataPoint<T> {
         return Date(timeIntervalSinceReferenceDate: timeInterval)
     }
     
-    
+    /// Creates a new instance of a `DataPoint`
+    /// - Parameters:
+    ///   - value: The value captured
+    ///   - timeInterval: The time the value was captured
+    public init(value: T, timeInterval: TimeInterval) {
+        self.value = value
+        self.timeInterval = timeInterval
+    }
 }
